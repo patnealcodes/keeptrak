ca81299db3f4cb4ecb19bb81c917d41e0e0d9c6a
Patrick Neal <pat.neal@rocketinsights.com>
2019-11-11 09:02:29 -0500

    Installed Material UI & some tweaks

--- src/shared/Reducers.ts
+++ src/shared/Reducers.ts
@@ -1,26 +1,44 @@
 import { IState, IAction } from './Types';
 import Actions from './Actions';
 
-export const infoReducer = (state: IState, action: IAction) => {
-  switch (action.type) {
+export const infoReducer = (state: IState, { type, payload }: IAction) => {
+  switch (type) {
     case Actions.ADD_PROJECT:
-      break;
+      console.log('ADD_PROJECT dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.UPDATE_PROJECT:
-      break;
+      console.log('UPDATE_PROJECT dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.DELETE_PROJECT:
-      break;
+      console.log('DELETE_PROJECT dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.ADD_TAG:
-      break;
+      console.log('ADD_TAG dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.UPDATE_TAG:
-      break;
+      console.log('UPDATE_TAG dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.DELETE_TAG:
-      break;
+      console.log('DELETE_TAG dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.ADD_TASK_ITEM:
-      break;
+      console.log('ADD_TASK_ITEM dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.UPDATE_TASK_ITEM:
-      break;
+      console.log('UPDATE_TASK_ITEM dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
     case Actions.DELETE_TASK_ITEM:
-      break;
+      console.log('DELETE_TASK_ITEM dispatched.');
+      console.log('payload: ', payload);
+      return { ...state };
 
     default:
       break;
